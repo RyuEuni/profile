48256d5f4e068772bd98e5be6e0b53ec492664c8
euni.ryu <euni.ryu97@gmail.com>
2023-07-13 17:43:17 +0900

    프로필 제작1

--- pfPage.js
+++ pfPage.js
@@ -1,1 +1,34 @@
+
+function tagCreate(tType, props) {
+  let element = document.createElement(tType);
+  for (let i in props) {
+    element[i] = props[i];
+  }
+  return element;
+};
+
+function styleCreate(obj, styleOb) {
+  for (i in styleOb) {
+    obj.style[i] = styleOb[i];
+  }
+}
+
 let root = document.getElementById('root');
+styleCreate(root, {
+  width: '100vm',
+  height: '100vh',
+  backgroundColor: '#333',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
+
+let cardBody = tagCreate('div', {})
+root.appendChild(cardBody)
+styleCreate(cardBody, {
+  width: '350px',
+  height: '550px',
+  backgroundColor: '#E6FAFF',
+  borderRadiues
+})
+
---
+++ public/func.js
@@ -0,0 +1,16 @@
+// function tagCreate(tType, props) {
+//   let element = document.createElement(tType);
+//   for (let i in props) {
+//     element[i] = props[i];
+//   }
+//   return element;
+// };
+
+// function styleCreate(obj, styleOb) {
+//   for (i in styleOb) {
+//     obj.style[i] = styleOb[i];
+//   }
+// }
+
+// exports.tagCreate = tagCreate;
+// exports.styleCreate = styleCreate;
